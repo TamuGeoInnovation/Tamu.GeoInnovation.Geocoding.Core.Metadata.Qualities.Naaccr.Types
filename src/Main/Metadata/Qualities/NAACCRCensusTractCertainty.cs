04b8c861919b7c285c3ebc3907b55b1e07a6eaa3
dwgoldbe <daniel.goldberg@tamu.edu>
2015-06-18 17:04:59 -0500

    Initial Commit

---
+++ src/Main/Metadata/Qualities/NAACCRCensusTractCertainty.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using USC.GISResearchLab.Census.Core.Configurations.ServerConfigurations;
+using USC.GISResearchLab.Common.Addresses.AbstractClasses;
+
+using USC.GISResearchLab.Geocoding.Core.Queries.Parameters;
+
+namespace USC.GISResearchLab.Geocoding.Core.Metadata.Qualities
+{
+
+    public enum NAACCRCensusTractCertaintyType
+    {
+        Unknown,
+        ResidenceStreetAddress,
+        ResidenceZIPPlus4,
+        ResidenceZIPPlus2,
+        ResidenceZIP,
+        POBoxZIP,
+        ResidenceCityOrZIPWithOneCensusTract,
+        Missing,
+        Unmatchable,
+        NotAttempted,
+    }
+}
---
+++ src/Main/Metadata/Qualities/NAACCRGISCoordinateQuality.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using USC.GISResearchLab.Common.Addresses.AbstractClasses;
+
+
+namespace USC.GISResearchLab.Geocoding.Core.Metadata.Qualities
+{
+
+    public enum NAACCRGISCoordinateQualityType
+    {
+        Unknown,
+        AddressPoint,
+        GPS,
+        Parcel,
+        StreetSegmentInterpolation,
+        StreetIntersection,
+        StreetCentroid,
+        AddressZIPPlus4Centroid,
+        AddressZIPPlus2Centroid,
+        ManualLookup,
+        AddressZIPCentroid,
+        POBoxZIPCentroid,
+        CityCentroid,
+        CountyCentroid,
+        Unmatchable,
+        Missing
+    }
+}
